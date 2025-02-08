5a0e3f143314d993b6254903a44b35e7fd9243c9
motolaji <jajimalik153@gmail.com>
2025-02-08 23:59:57 +0000

    eighth commit

--- contacts_project/contacts_api/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Contact, Category
-
-# Register your models here.
-
-admin.site.register(Contact)
-admin.site.register(Category)
--- contacts_project/contacts_api/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ContactsApiConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "contacts_api"
